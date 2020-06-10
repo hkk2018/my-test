5ba2aa89c3c0c9367eab30fe35861774aedddda1
hongkaikao@gmail.com <hongkaikao@gmail.com>
2020-06-10 17:18:49 +0800

    2020-0610 1.ps

---
+++ ps/pack.ps1
@@ -0,0 +1,33 @@
+﻿#--- Optional Params
+$folderName = "cheerleaderImgs" #留空則會詢問
+$autoClose = 10; 
+
+
+#--- Basics
+$backupPrefix = Read-Host -Prompt '請輸入備份前綴(如日期、功能等)'
+
+
+if ($folderName -eq "") {
+    $folderName = Read-Host -Prompt '請輸入備份名稱'
+}
+$backupFullName = "[$backupPrefix]$folderName"
+$folderLocation = ""
+# $folderPath = $folderLocation + $folderName;
+
+#--- Winrar
+$rarProgramPath = "C:\Program Files\WinRAR\Rar.exe"
+$desitination = $backupFullName + '.rar'; # 產生於working directory
+$source = $folderName
+Get-Item -LiteralPath $desitination | Remove-Item # 有就會刪除
+$argumentStr = "a $desitination $source" # 壓縮前最好先移除現有的同名壓縮檔, 因為目標中多的檔案不會被移除
+
+
+# cmd中是
+#   -a destination source
+# 但ps則不加減號
+Start-Process -FilePath $rarProgramPath -ArgumentList $argumentStr -Wait
+
+"壓縮成功～！"
+"$autoClose 秒後程序將自動關閉"
+Start-Sleep $autoClose
+Exit
