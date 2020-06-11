35ea076b8f3363c50664fed2b0beb54f49aed5c8
hongkaikao@gmail.com <hongkaikao@gmail.com>
2020-06-11 13:42:36 +0800

    2020-0611 1.improve pack.ps1

--- ps/pack.ps1
+++ ps/pack.ps1
@@ -1,14 +1,16 @@
 ﻿#--- Optional Params
 $folderName = "cheerleaderImgs" #留空則會詢問
-$autoClose = 10; 
+$autoCloseDelay = 15; 
 
 
 #--- Basics
+Write-Host "將備份資料夾: $folderName"
+""
 $backupPrefix = Read-Host -Prompt '請輸入備份前綴(如日期、功能等)'
 
 
 if ($folderName -eq "") {
-    $folderName = Read-Host -Prompt '請輸入備份名稱'
+    $folderName = Read-Host -Prompt '請輸入備份資料夾名稱'
 }
 $backupFullName = "[$backupPrefix]$folderName"
 $folderLocation = ""
@@ -21,10 +23,20 @@ $source = $folderName
 Get-Item -LiteralPath $desitination | Remove-Item # 有就會刪除，壓縮前最好先移除現有的同名壓縮檔, 因為目標中多的檔案不會被移除
 $argumentStr = "a $desitination $source" # cmd中是 -a destination source ，但ps則不加減號
 
-
+# 壓縮
 Start-Process -FilePath $rarProgramPath -ArgumentList $argumentStr -Wait
 
+# 計算檔案大小(MB)
+$rarFile=Get-Item -LiteralPath $desitination
+$fileSize=$rarFile.Length/1MB
+$formattedSize='{0:n3}' -f $fileSize # https://www.pstips.net/formatting-numbers-easily.html
+
+
+""
 "壓縮成功～！"
-"$autoClose 秒後程序將自動關閉"
-Start-Sleep $autoClose
+""
+"檔名為 $desitination ，大小為 $formattedSize MB"
+""
+"$autoCloseDelay 秒後程序將自動關閉"
+Start-Sleep $autoCloseDelay
 Exit
